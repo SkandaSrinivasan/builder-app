078d3c61dafccb36442f962a2296b1df4060bd7f
Skanda Srinivasan <srinivasan.sk@northeastern.edu>
2021-10-02 00:45:25 -0400

    test commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [defaultValue, setDefaultValue] = useState("");
+  const [label, setLabel] = useState("");
+  const [choiceText, setChoiceText] = useState("");
+  const handleLabelChange = (evt) => {
+    setLabel(evt.target.value);
+  };
+
+  const handleDefValueChange = (evt) => {
+    setDefaultValue(evt.target.value);
+  };
+
+  const handleChoiceTextChanges = (evt) => {
+    setChoiceText(evt.target.value);
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(choiceText.split(/\n/));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Label:
+          <input
+            value={label}
+            onChange={handleLabelChange}
+            type="text"
+            name="label"
+          />
+        </label>
+        <label>
+          Type:
+          <select name="seltype">
+            <option value="multi-select">multi-select</option>
+            <option value="single-select">single-select</option>
+          </select>
+        </label>
+        <label>
+          Default Value:
+          <input
+            value={defaultValue}
+            onChange={handleDefValueChange}
+            type="text"
+            name="defaultvalue"
+          />
+        </label>
+        <label>
+          Choices
+          <textarea 
+          value={choiceText} 
+          onChange={handleChoiceTextChanges}
+          cols={5}></textarea>
+        </label>
+        <label>
+          Order:
+          <select name="seltype">
+            <option value="alphabetical">Display choices in alphabetical</option>
+            <option value="whatever">whatever the fk</option>
+          </select>
+        </label>
+        <button type="submit">Save Changes</button>
+      </form>
     </div>
   );
 }
